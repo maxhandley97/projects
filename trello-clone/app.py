efca0edef5c15ff22ba4f49f05b178463ec71842
maxhandley97 <maxhandley97@gmail.com>
2023-11-22 14:09:48 +1100

    added JWT tokens

--- trello-clone/app.py
+++ trello-clone/app.py
@@ -3,15 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import JWTManager, create_access_token #make sure valid token is parsed through with request
+from datetime import timedelta
 
 app = Flask(__name__)
-
+app.config['JWT_SECRET_KEY'] = 'Ministry of Silly Walks'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@127.0.0.1:5432/trello' #database connection string
 # avoid depreciation warning
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app) #connects two
 ma = Marshmallow(app) 
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 class Card(db.Model): #extends db.model
     __tablename__ = 'cards' #dunder to name table, default is singular card
@@ -95,20 +99,43 @@ def db_seed():
     
 @app.route('/users/register', methods=['POST'])
 def register():
-    #Parse incoming POST body through schema
-    user_info = UserSchema(exclude=['id']).load(request.json)
-    #create new user with parsed data
-    user = User(
-        email=user_info['email'],
-        password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
-        name=user_info.get('name', '')
-    )
-    db.session.add(user) #add and commmit new user to the database
-    db.session.commit()
-    # print(request.json) - used for 
-    # print(user.__dict__)
-    # return 'ok', 201
-    return UserSchema(exclude=['password']).dump(user), 201 #return the new user
+    try:
+        #Parse incoming POST body through schema
+        user_info = UserSchema(exclude=['id']).load(request.json)
+        #create new user with parsed data
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
+            name=user_info.get('name', '')
+        )
+        db.session.add(user) #add and commmit new user to the database
+        db.session.commit()
+        # print(request.json) - used for 
+        # print(user.__dict__)
+        # return 'ok', 201
+        return UserSchema(exclude=['password']).dump(user), 201 #return the new user, dump instead of dumps because the header will 
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
+
+@app.route('/users/login', methods=['POST'])
+def login():
+    #1. parse incoming POST body through the schema
+    user_info = UserSchema(exclude=['id', 'name', 'is_admin']).load(request.json)
+    #2. select user from db, with email that matches the one in the post body, can print these steps to debug
+    stmt = db.select(User).where(User.email == user_info['email'])
+    user = db.session.scalar(stmt)
+    #3. Check the password hash
+    if user and bcrypt.check_password_hash(user.password, user_info['password']): #method of bcrypt, checks hash password in db and user made posswor
+        #4. Create JWT Token
+        token = create_access_token(identity=user.email, expires_delta=timedelta(hours=2))
+        return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
+    else:
+        return {'error': 'Invalid email or password'}, 401
+    print(user)
+    #3. Check the password hash
+    #4. Create JWT Token
+    #5. Return the token
+    return 'ok'
 
 @app.route('/users/<userId>', methods=['PATCH'])
 def update_user(userId):
@@ -122,6 +149,7 @@ def update_user(userId):
     # print(user.__dict__)
     # return 'ok', 201
     return UserSchema(exclude=['password']).dump(user), 201 #return the new user
+
 
 @app.route('/users/<userId>', methods=['DELETE'])
 def delete_user(userId):
@@ -138,13 +166,16 @@ def delete_user(userId):
 @app.route('/cards')
 def all_cards():
     # select * from cards in stmt variable to ;
-    stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc()) #create statement sql
+    # stmt = db.select(Card).where(db.or_(Card.status != 'Done', Card.id > 2)).order_by(Card.title.desc()) #create statement sql
+    stmt = db.select(Card)
     cards = db.session.scalars(stmt).all() #execute method, scalars to have singular objects/model instances
     return CardSchema(many=True).dump(cards) #flask takes care of serialisation
 
 @app.route('/')
 def index():
     return 'Hello, world'
-
+@app.errorhandler(IntegrityError)
+def integrity_error(err):
+    return {'error': str(err)}, 409
 #so basically flask allows us to create route that can be access by clients and then 
 # flask sqlalchemy + marshmallow allows us to create object out of our database to manipulate and then convert it to json so that the data can be transmitted via the routes and therefore that's the transition of data ?
